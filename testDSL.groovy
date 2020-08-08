a3eaf55f535e70e6003b72d7cf5045b0865ff5ad
Pratik Das <pratik.das@hotmail.com>
2020-08-08 09:44:12 +0530

    Create testDSL.groovy

---
+++ testDSL.groovy
@@ -0,0 +1,16 @@
+freeStyleJob('example') {
+    logRotator(-1, 10)
+    scm {
+        git {
+            branch('master')
+            remote {
+                url("https://bitbucket.org/ram_kannan/devops_app_e2e_workflow.git")
+            }
+        }
+    }
+    
+    steps {
+        shell('mvn clean install')
+    }
+    
+}
